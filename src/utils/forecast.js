511efd94986ab8e73610bf32715a5ab943bac1f5
Your Name <you@example.com>
2019-04-28 10:38:16 +0300

    Add new forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,8 @@ request({url, json:true}, (error, {body})=>{
         callback('Coordinate Error! Unable to find location', undefined)
             
     } else{
-        callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+       
+        callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain. The timezone is '+body.timezone)
     }
 }) 
 
